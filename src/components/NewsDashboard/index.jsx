510f536ebb6e836e443a4acfbae77764793a4971
wyy998 <1490473564@qq.com>
2019-10-21 23:20:11 +0800

    update content

--- src/components/NewsDashboard/index.jsx
+++ src/components/NewsDashboard/index.jsx
@@ -8,7 +8,8 @@ class NewsDashboard extends React.Component {
       title: "Hahah",
       content: [
         "This is a joke",
-        "在正常的css中，比如background-color，box-sizing等属性，在style对象div1中的属性中，必须转换成驼峰法，backgroundColor，boxSizing。而没有连字符的属性，如margin，width等，则在style对象中不变。作者：grain先森"
+        "在正常的css中，比如background-color，box-sizing等属性，在style对象div1中的属性中，必须转换成驼峰法，backgroundColor，boxSizing。而没有连字符的属性，如margin，width等，则在style对象中不变。",
+        "作者：grain先森"
       ]
     };
   }
